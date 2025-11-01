326bf95e674add76702eac19cbd7bd216ceb5fe8
lauraGalviz <laura.galviz@finanzauto.com>
2025-11-01 10:35:37 -0500

    nuevo

--- src/db.js
+++ src/db.js
@@ -3,12 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const connectMongoDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ Conectado a MongoDB Atlas (AppsU)");
-  } catch (error) {
-    console.error("❌ Error al conectar a MongoDB:", error);
-    process.exit(1);
+export const connectMongoDB = () => {
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+      console.log("✅ Conectado a MongoDB Atlas (AppsU)");
+    })
+    .catch((error) => {
+      console.error("❌ Error al conectar a MongoDB:", error);
+      process.exit(1);
+    })
+    .finally(() => {
+      console.log("🔄 Intento de conexión finalizado.");
+    });
   }
-};
