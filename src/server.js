87138be5ece83a98e99ad28f37869998c09665ef
lauraGalviz <laura.galviz@finanzauto.com>
2025-11-15 10:23:28 -0500

    sin sql

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ import cors from "cors";
 import { connectMongoDB } from "./db.js";
 import { swaggerUi, swaggerSpec } from "./config/swagger.js";
 import hunterRoutes from "./routes/hunterRoutes.js"
-import hunterSqlRoutes from "./routes/hunterSqlRoutes.js";
-import hunterMergeRoutes from "./routes/hunterMergeRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 
 
@@ -22,8 +20,6 @@ app.use(cors({
 
 app.use("/api/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/hunters", hunterRoutes); 
-app.use("/api/hunters-sql", hunterSqlRoutes);
-app.use("/api/huntersMerge", hunterMergeRoutes)
 app.use("/api/users", userRoutes)
 
 
